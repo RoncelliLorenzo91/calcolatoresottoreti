12ac4fe5df85a6557e13a884cc7d7cb6f64e7a27
Loryr <Loryr@MSI>
2023-11-03 16:43:55 +0100

    modifiche

--- calcolatoresottoreti/Form1.Designer.cs
+++ calcolatoresottoreti/Form1.Designer.cs
@@ -28,54 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numeroHost = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.numeroSottoreti = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtClasseIndirizzo = new System.Windows.Forms.TextBox();
+            this.txtIndirizzoBase = new System.Windows.Forms.TextBox();
+            this.txtSubnetMask = new System.Windows.Forms.TextBox();
+            this.txtCDIR = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // numeroHost
             // 
-            this.textBox1.Location = new System.Drawing.Point(39, 84);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(156, 20);
-            this.textBox1.TabIndex = 0;
+            this.numeroHost.Location = new System.Drawing.Point(38, 51);
+            this.numeroHost.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numeroHost.Name = "numeroHost";
+            this.numeroHost.Size = new System.Drawing.Size(207, 22);
+            this.numeroHost.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label1.Location = new System.Drawing.Point(36, 51);
+            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 18);
+            this.label1.Size = new System.Drawing.Size(203, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Inserire numero di host";
             // 
-            // textBox2
+            // numeroSottoreti
             // 
-            this.textBox2.Location = new System.Drawing.Point(39, 182);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(156, 20);
-            this.textBox2.TabIndex = 2;
+            this.numeroSottoreti.Location = new System.Drawing.Point(38, 135);
+            this.numeroSottoreti.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numeroSottoreti.Name = "numeroSottoreti";
+            this.numeroSottoreti.Size = new System.Drawing.Size(207, 22);
+            this.numeroSottoreti.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label2.Location = new System.Drawing.Point(36, 152);
+            this.label2.Location = new System.Drawing.Point(13, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(184, 18);
+            this.label2.Size = new System.Drawing.Size(232, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Inserire numero di sottoreti";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.button1.Location = new System.Drawing.Point(39, 234);
+            this.button1.Location = new System.Drawing.Point(317, 200);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 104);
+            this.button1.Size = new System.Drawing.Size(208, 128);
             this.button1.TabIndex = 4;
             this.button1.Text = "calcola";
             this.button1.UseVisualStyleBackColor = true;
@@ -84,23 +97,101 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(355, 46);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(584, 13);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(382, 368);
+            this.listBox1.Size = new System.Drawing.Size(470, 452);
             this.listBox1.TabIndex = 5;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtClasseIndirizzo
+            // 
+            this.txtClasseIndirizzo.Location = new System.Drawing.Point(38, 219);
+            this.txtClasseIndirizzo.Name = "txtClasseIndirizzo";
+            this.txtClasseIndirizzo.Size = new System.Drawing.Size(207, 22);
+            this.txtClasseIndirizzo.TabIndex = 6;
+            // 
+            // txtIndirizzoBase
+            // 
+            this.txtIndirizzoBase.Location = new System.Drawing.Point(38, 306);
+            this.txtIndirizzoBase.Name = "txtIndirizzoBase";
+            this.txtIndirizzoBase.Size = new System.Drawing.Size(207, 22);
+            this.txtIndirizzoBase.TabIndex = 7;
+            // 
+            // txtSubnetMask
+            // 
+            this.txtSubnetMask.Location = new System.Drawing.Point(38, 390);
+            this.txtSubnetMask.Name = "txtSubnetMask";
+            this.txtSubnetMask.Size = new System.Drawing.Size(207, 22);
+            this.txtSubnetMask.TabIndex = 8;
+            // 
+            // txtCDIR
+            // 
+            this.txtCDIR.Location = new System.Drawing.Point(38, 493);
+            this.txtCDIR.Name = "txtCDIR";
+            this.txtCDIR.Size = new System.Drawing.Size(207, 22);
+            this.txtCDIR.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(81, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "classe indirizzo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.Location = new System.Drawing.Point(81, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Indirizzo base:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label5.Location = new System.Drawing.Point(87, 363);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Subnet Mask:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label6.Location = new System.Drawing.Point(114, 456);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "CIDR:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCDIR);
+            this.Controls.Add(this.txtSubnetMask);
+            this.Controls.Add(this.txtIndirizzoBase);
+            this.Controls.Add(this.txtClasseIndirizzo);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.numeroSottoreti);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numeroHost);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -111,12 +202,20 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox numeroHost;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox numeroSottoreti;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtClasseIndirizzo;
+        private System.Windows.Forms.TextBox txtIndirizzoBase;
+        private System.Windows.Forms.TextBox txtSubnetMask;
+        private System.Windows.Forms.TextBox txtCDIR;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- calcolatoresottoreti/Form1.cs
+++ calcolatoresottoreti/Form1.cs
@@ -23,16 +23,91 @@ namespace calcolatoresottoreti
         }
 
         private void button1_Click(object sender, EventArgs e)
-        { 
-        
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Leggi il numero di sottoreti e il numero massimo di host da input
+            int numSottoreti = int.Parse(numeroSottoreti.Text);
+            int numHostPerSottorete = int.Parse(numeroHost.Text);
 
+            // Genera un indirizzo IP casuale per la rete
+            Random random = new Random();
+            string indirizzoRete = random.Next(1, 256) + "." + random.Next(0, 256) + "." + random.Next(0, 256) + "." + random.Next(1, 256);
+
+            // Calcola la classe dell'indirizzo IP casuale
+            string classe = CalcolaClasse(indirizzoRete);
+
+            // Calcola il CIDR e la subnet mask in base ai dati
+            int bitsHost = (int)Math.Ceiling(Math.Log(numHostPerSottorete + 2) / Math.Log(2));
+            int bitsSottoreti = (int)Math.Ceiling(Math.Log(numSottoreti) / Math.Log(2));
+            int bitsRete = 32 - bitsSottoreti - bitsHost;
+
+            string cdir = $"/{bitsRete}";
+            string subnetMask = CalcolaSubnetMask(bitsRete);
+
+            // Calcola il piano di indirizzamento per tutte le sottoreti
+            listBox1.Items.Clear(); // Pulisci i risultati precedenti
+
+            for (int i = 0; i < numSottoreti; i++)
+            {
+                int rete = i;
+                int host = i % numHostPerSottorete;
+
+                string indirizzoHost = string.Format("{0}.{1}.{2}.{3}",
+                    indirizzoRete.Split('.')[0],
+                    indirizzoRete.Split('.')[1],
+                    indirizzoRete.Split('.')[2],
+                    (int.Parse(indirizzoRete.Split('.')[3]) + host));
+
+                listBox1.Items.Add($"Sottorete {i + 1}: Rete {indirizzoRete}, Host {indirizzoHost}");
+            }
+
+            // Aggiorna i risultati nella GUI
+            txtClasseIndirizzo.Text = classe;
+            txtIndirizzoBase.Text = indirizzoRete;
+            txtSubnetMask.Text = subnetMask;
+            txtCDIR.Text = cdir;
         }
+        private string CalcolaClasse(string indirizzo)
+        {
+            int primoByte = int.Parse(indirizzo.Split('.')[0]);
+
+            if (primoByte >= 1 && primoByte <= 126)
+            {
+                return "Classe A";
+            }
+            else if (primoByte >= 128 && primoByte <= 191)
+            {
+                return "Classe B";
+            }
+            else if (primoByte >= 192 && primoByte <= 223)
+            {
+                return "Classe C";
+            }
+            else if (primoByte >= 224 && primoByte <= 239)
+            {
+                return "Classe D";
+            }
+            else if (primoByte >= 240 && primoByte <= 255)
+            {
+                return "Classe E";
+            }
+            else
+            {
+                return "Classe sconosciuta";
+            }
+        }
+
+        private string CalcolaSubnetMask(int bitsRete)
+{
+    uint subnetMaskValue = 0xFFFFFFFF << (32 - bitsRete);
+    byte[] subnetMaskBytes = BitConverter.GetBytes(subnetMaskValue);
+    Array.Reverse(subnetMaskBytes); // Dovremmo invertire l'ordine dei byte
+
+    string subnetMask = string.Join(".", subnetMaskBytes);
+    return subnetMask;
+}
     }
 }
+    
 
          
 
